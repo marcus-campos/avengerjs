7b683e9f85be1ef88d8c506f7ae5dd276088b9c3
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-02-14 17:54:11 -0200

    Remove morgan

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path           = require('path')
 
 // Dependencies
 const favicon        = require('serve-favicon')
-const logger         = require('morgan')
 const cookieParser   = require('cookie-parser')
 const bodyParser     = require('body-parser')
 
