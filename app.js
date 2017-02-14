1ae218d3dd79b05b1b4e01316d444d6e0d5327d2
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-02-14 09:43:22 -0200

    Initial provider set

--- app.js
+++ app.js
@@ -37,48 +37,6 @@ app.use(helmet())
 app.use('/', index)
 
 
-/*
- * HELMET Configs
- */
-
-// Disable Headers
-app.disable('x-powered-by')
-
-// Implement CSP with Helmet
-app.use(helmet.contentSecurityPolicy(helmetConfig.csp))
-
-// Implement X-XSS-Protection
-app.use(helmet.xssFilter());
-
-
-// Implement X-Frame
-//app.use(helmet.xframe());
-//app.use(helmet.xframe('deny'));
-//app.use(helmet.xframe('sameorigin'));
-//app.use(helmet.xframe('allow-from', 'http://example.com'));
-
-// Implement Strict-Transport-Security
-app.use(helmet.hsts({
-    maxAge: 7776000000,
-    includeSubdomains: true
-}));
-
-// Sets "Referrer-Policy: same-origin".
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
-
-// Implement hpkp for HTTP Public Key Pinning
-
-const ninetyDaysInSeconds = 7776000
-
-app.use(helmet.hpkp({
-    maxAge: ninetyDaysInSeconds,
-    sha256s: ['plU644p51=', 'zUk5tAtU5=']
-}))
-
-/*
- * END HELMET Configs
- */
-
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
   let err = new Error('Not Found')
